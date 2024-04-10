31ba964067be3bfc845d2586e7073b60b14a4c16
David Arabuli <davidsarabuli@gmail.com>
2024-04-10 14:20:11 +0300

    load from Local Storage implemented

--- localStorageFuncs.js
+++ localStorageFuncs.js
@@ -3,7 +3,7 @@ import { prepareUnitsData } from "./gatherData.js";
 export function saveToLocalStorage() {
   const form = document.querySelector("#Units");
   const inputValues = form.querySelectorAll("input");
-  console.log(inputValues);
+
   const unitsData = prepareUnitsData(inputValues);
   localStorage.setItem("unitsData", JSON.stringify(unitsData));
 }
@@ -13,7 +13,32 @@ export function loadFromLocStorage() {
   const unitsObjects = JSON.parse(retrievedData);
   console.log("retrieved data");
   console.log(unitsObjects);
+  return unitsObjects;
 }
+
+export function repopulateInputsFromLocStor(parsedData) {
+  document.querySelectorAll("input").forEach((input) => {
+    parsedData.forEach((obj) => {
+      if (input.dataset.id === obj.id) {
+        switch (input.name) {
+          case "attackPrimary":
+            input.value = obj.attackPrimary;
+            break;
+          case "attackSecondary":
+            input.value = obj.attackSecondary;
+            break;
+          case "defence":
+            input.value = obj.defence;
+            break;
+
+          default:
+            break;
+        }
+      }
+    });
+  });
+}
+
 export function localStorageSetup() {
   const saveBtn = document.querySelector("#saveBtn");
   saveBtn.addEventListener("click", () => {
@@ -21,6 +46,6 @@ export function localStorageSetup() {
   });
   const loadBtn = document.querySelector("#loadBtn");
   loadBtn.addEventListener("click", () => {
-    loadFromLocStorage();
+    repopulateInputsFromLocStor(loadFromLocStorage());
   });
 }
